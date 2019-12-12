8d13ac19234efc5bec4aad3c0dd402a8e7cd6c26
gkatjstod <58029482+gkatjstod@users.noreply.github.com>
2019-12-12 19:41:24 +0900

    반전수정 재탕입니다

--- proto_ver2+Cam/Assets/Script/MoveAnimation.cs
+++ proto_ver2+Cam/Assets/Script/MoveAnimation.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Spine.Unity;
 
+
 public class MoveAnimation : MonoBehaviour
 {
     ///애니메이션 상태를 enum으로 체크
@@ -28,13 +29,14 @@ public class MoveAnimation : MonoBehaviour
         back = 2,
         left = 3,
     }
-
+  
+   
     public ANGLE_TYPE angletype;
-    
+
 
 
     SkeletonAnimation SkeletonAnimation;
-    public GameObject Hitbox;
+   
     Rigidbody Myrigidbody;
     BoxCollider HitboxCollider;
 
@@ -57,31 +59,118 @@ public class MoveAnimation : MonoBehaviour
     public float angle = 90.0f;
     private Quaternion _targetRot = Quaternion.identity;
     private bool _isForward = true;
-    /// 
+    private bool _isReverse = false;//애니메이션 좌우반전 확인용
+                                    /// 
+    public int PlayerNumber;//플레이어넘버
 
     ACTION_TYPE atype;
-    bool IsPadCenter() //L스틱중립
+    //현재는 쓰지않는 조건
+    /*
+      bool IsPadCenter() //L스틱중립
     {return ((PrePadH == 0) && (Input.GetAxisRaw("LeftStickHorizontal") == 0) && (Input.GetAxisRaw("LeftStickVertical") == 0) && (PrePadV == 0));}
 
+        bool IsPadLPressing()//L스틱 좌입력중
+    {return ((PrePadH == -1) && (Input.GetAxisRaw("LeftStickHorizontal") == -1));}
+        bool IsPadRPressing()//L스틱 우입력중
+    {return ((PrePadH == 1) && (Input.GetAxisRaw("LeftStickHorizontal") == 1));}
+     */
+
+        //사용중
     bool IsPadLDown()//L스틱 좌입력
-    {return ((PrePadH > Input.GetAxisRaw("LeftStickHorizontal")) && (Input.GetAxisRaw("LeftStickHorizontal")<0) );}
+    {
+        switch (PlayerNumber)
+        {
+            case 1:
+                return ((PrePadH > Input.GetAxisRaw("P1LeftStickHorizontal")) && (Input.GetAxisRaw("P1LeftStickHorizontal") < 0));
+                break;
+            case 2:
+                return ((PrePadH > Input.GetAxisRaw("P2LeftStickHorizontal")) && (Input.GetAxisRaw("P2LeftStickHorizontal") < 0));
+                break;
+            case 3:
+                return ((PrePadH > Input.GetAxisRaw("P3LeftStickHorizontal")) && (Input.GetAxisRaw("P3LeftStickHorizontal") < 0));
+                break;
+            case 4:
+                return ((PrePadH > Input.GetAxisRaw("P4LeftStickHorizontal")) && (Input.GetAxisRaw("P4LeftStickHorizontal") < 0));
+                break;
+            default: break;
+        }
+        return false;
+      //  return ((PrePadH > Input.GetAxisRaw("LeftStickHorizontal")) && (Input.GetAxisRaw("LeftStickHorizontal") < 0));
+    }
 
     bool IsPadLUp()//L스틱 좌입력 해제
-    {return ((PrePadH < 0) && (Input.GetAxisRaw("LeftStickHorizontal")==0) );}
+    {
+        switch (PlayerNumber)
+        {
+            case 1:
+                return ((PrePadH < 0) && (Input.GetAxisRaw("P1LeftStickHorizontal") == 0));
+                break;
+            case 2:
+                return ((PrePadH < 0) && (Input.GetAxisRaw("P2LeftStickHorizontal") == 0));
+                break;
+            case 3:
+                return ((PrePadH < 0) && (Input.GetAxisRaw("P3LeftStickHorizontal") == 0));
+                break;
+            case 4:
+                return ((PrePadH < 0) && (Input.GetAxisRaw("P4LeftStickHorizontal") == 0));
+                break;
+            default: break;
+        }
+        return false;
+        //return ((PrePadH < 0) && (Input.GetAxisRaw("LeftStickHorizontal")==0) );
+    }
 
-    bool IsPadLPressing()//L스틱 좌입력중
-    {return ((PrePadH == -1) && (Input.GetAxisRaw("LeftStickHorizontal") == -1));}
+    
 
     bool IsPadRDown()//L스틱 우입력
-    {return ((PrePadH < Input.GetAxisRaw("LeftStickHorizontal")) && (Input.GetAxisRaw("LeftStickHorizontal") >0));}
+    {
+        switch (PlayerNumber)
+        {
+            case 1:
+                return ((PrePadH < Input.GetAxisRaw("P1LeftStickHorizontal")) && (Input.GetAxisRaw("P1LeftStickHorizontal") > 0));
+                break;
+            case 2:
+                return ((PrePadH < Input.GetAxisRaw("P2LeftStickHorizontal")) && (Input.GetAxisRaw("P2LeftStickHorizontal") > 0));
+                break;
+            case 3:
+                return ((PrePadH < Input.GetAxisRaw("P3LeftStickHorizontal")) && (Input.GetAxisRaw("P3LeftStickHorizontal") > 0));
+                break;
+            case 4:
+                return ((PrePadH < Input.GetAxisRaw("P4LeftStickHorizontal")) && (Input.GetAxisRaw("P4LeftStickHorizontal") > 0));
+                break;
+            default: break;
+        }
+        return false;
+        //   return ((PrePadH < Input.GetAxisRaw("LeftStickHorizontal")) && (Input.GetAxisRaw("LeftStickHorizontal") >0));
+    }
 
     bool IsPadRUp()//L스틱 우입력 해제
-    {return ((PrePadH > 0) && (Input.GetAxisRaw("LeftStickHorizontal") == 0));}
+    {
+        switch (PlayerNumber)
+        {
+            case 1:
+                return ((PrePadH > 0) && (Input.GetAxisRaw("P1LeftStickHorizontal") == 0));
+                break;
+            case 2:
+                return ((PrePadH > 0) && (Input.GetAxisRaw("P2LeftStickHorizontal") == 0));
+                break;
+            case 3:
+                return ((PrePadH > 0) && (Input.GetAxisRaw("P3LeftStickHorizontal") == 0));
+                break;
+            case 4:
+                return ((PrePadH > 0) && (Input.GetAxisRaw("P4LeftStickHorizontal") == 0));
+                break;
+            default: break;
+        }
+        return false;
+        // return ((PrePadH > 0) && (Input.GetAxisRaw("LeftStickHorizontal") == 0));
+    }
 
-    bool IsPadRPressing()//L스틱 우입력중
-    {return ((PrePadH == 1) && (Input.GetAxisRaw("LeftStickHorizontal") == 1));}
 
 
+  
+
+   
 
     
     void Start()
@@ -91,9 +180,28 @@ public class MoveAnimation : MonoBehaviour
         Myrigidbody = GetComponent<Rigidbody>();
 
         SetActionType(ACTION_TYPE.ch_idle);
-        PrePadH = Input.GetAxisRaw("LeftStickHorizontal");
+        switch (PlayerNumber)
+        {
+            case 1:
+                PrePadH = Input.GetAxisRaw("P1LeftStickHorizontal");
+                break;
+            case 2:
+                PrePadH = Input.GetAxisRaw("P2LeftStickHorizontal");
+                break;
+            case 3:
+                PrePadH = Input.GetAxisRaw("P3LeftStickHorizontal");
+                break;
+            case 4:
+                PrePadH = Input.GetAxisRaw("P4LeftStickHorizontal");
+                break;
+            default: break;
+        }
+        
         PrePadV = Input.GetAxisRaw("LeftStickVertical");
         Intervel = 0.0f;
+       // PlayerNumber = 0;
+        angletype = ANGLE_TYPE.front;
+        //angletype = front;
     }
 
     ///ACTION_TYPE별 애니메이션 정의
@@ -154,17 +262,17 @@ public class MoveAnimation : MonoBehaviour
             SetActionType(ACTION_TYPE.ch_rightmove);
         }
 
-        if (Input.GetKeyDown(KeyCode.UpArrow) || IsPadLDown())//L스틱 좌입력
-        {
-            lookright = false;
-            SetActionType(ACTION_TYPE.ch_upmove);
-        }
+        //if (Input.GetKeyDown(KeyCode.UpArrow) || IsPadLDown())//L스틱 좌입력
+        //{
+        //    lookright = false;
+        //    SetActionType(ACTION_TYPE.ch_upmove);
+        //}
 
-        if (Input.GetKeyDown(KeyCode.DownArrow) || IsPadRDown())//L스틱 우입력
-        {
-            lookright = true;
-            SetActionType(ACTION_TYPE.ch_downmove);
-        }
+        //if (Input.GetKeyDown(KeyCode.DownArrow) || IsPadRDown())//L스틱 우입력
+        //{
+        //    lookright = true;
+        //    SetActionType(ACTION_TYPE.ch_downmove);
+        //}
         /*
         if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.Joystick1Button0))
         {
@@ -192,12 +300,26 @@ public class MoveAnimation : MonoBehaviour
         {
             case ACTION_TYPE.ch_leftmove:
                 {
-                    pos.x -= Time.deltaTime * speed;
+                    if(angletype==ANGLE_TYPE.front)
+                        pos.x -= Time.deltaTime * speed;
+                    if (angletype == ANGLE_TYPE.right)
+                        pos.z -= Time.deltaTime * speed;
+                    if (angletype == ANGLE_TYPE.back)
+                        pos.x += Time.deltaTime * speed;
+                    if (angletype == ANGLE_TYPE.left)
+                        pos.z += Time.deltaTime * speed;
                 }
                 break;
             case ACTION_TYPE.ch_rightmove:
                 {
-                    pos.x += Time.deltaTime * speed;
+                    if (angletype == ANGLE_TYPE.front)
+                       pos.x += Time.deltaTime * speed;
+                    if (angletype == ANGLE_TYPE.right)
+                        pos.z += Time.deltaTime * speed;
+                    if (angletype == ANGLE_TYPE.back)
+                        pos.x -= Time.deltaTime * speed;
+                    if (angletype == ANGLE_TYPE.left)
+                        pos.z -= Time.deltaTime * speed;
                 }
                 break;
 
@@ -218,6 +340,53 @@ public class MoveAnimation : MonoBehaviour
         transform.position = pos;
     }
 
+    // 좌우 캐릭터 방향전환
+    private void ChangeAngle()
+    {
+        if (Input.GetKeyDown(KeyCode.Q)||Input.GetKeyDown(KeyCode.JoystickButton4))
+        {
+            if (angletype == ANGLE_TYPE.front)
+            {
+                if (_isReverse ^ false)//정상
+                    _targetRot *= Quaternion.AngleAxis(_isForward ? -90 : 90, Vector3.up);
+                else//반전
+                    _targetRot *= Quaternion.AngleAxis(_isForward ? -270 : 270, Vector3.up);
+                angletype = ANGLE_TYPE.left;
+            }
+            else
+            {
+                if (_isReverse ^ false)//정상
+                    _targetRot *= Quaternion.AngleAxis(_isForward ? -90 : 90, Vector3.up);
+                else//반전
+                    _targetRot *= Quaternion.AngleAxis(_isForward ? -270 : 270, Vector3.up);
+                angletype -= 1;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.JoystickButton5))
+        {
+            if (angletype == ANGLE_TYPE.left)
+            {
+                if (_isReverse ^ false)//정상
+                    _targetRot *= Quaternion.AngleAxis(_isForward ? -90 : 90, Vector3.down);
+                else//반전
+                    _targetRot *= Quaternion.AngleAxis(_isForward ? -270 : 270, Vector3.down);
+                angletype = ANGLE_TYPE.front;
+            }
+
+            else
+            {
+                if (_isReverse ^ false)//정상
+                    _targetRot *= Quaternion.AngleAxis(_isForward ? -90 : 90, Vector3.down);
+                else//반전
+                    _targetRot *= Quaternion.AngleAxis(_isForward ? -270 : 270, Vector3.down);
+                angletype += 1;
+
+            }
+        }
+
+    }
+
     void Update()
     {
         Intervel += Time.deltaTime;
@@ -226,25 +395,30 @@ public class MoveAnimation : MonoBehaviour
         if (Intervel > 0.1f)
         {
 
-            PrePadH = Input.GetAxisRaw("LeftStickHorizontal");
+            switch (PlayerNumber)
+            {
+                case 1:
+                    PrePadH = Input.GetAxisRaw("P1LeftStickHorizontal");
+                    break;
+                case 2:
+                    PrePadH = Input.GetAxisRaw("P2LeftStickHorizontal");
+                    break;
+                case 3:
+                    PrePadH = Input.GetAxisRaw("P3LeftStickHorizontal");
+                    break;
+                case 4:
+                    PrePadH = Input.GetAxisRaw("P4LeftStickHorizontal");
+                    break;
+                default: break;
+            }
             PrePadV = Input.GetAxisRaw("LeftStickVertical");
             Intervel = 0.0f;
         }
 
 
         ///
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            _targetRot *= Quaternion.AngleAxis(_isForward ? -90 : 90, Vector3.up);
-            angletype -= 1;
-        }
-
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            _targetRot *= Quaternion.AngleAxis(_isForward ? -90 : 90, Vector3.down);
-            angletype += 1;
-
-        }
+        ChangeAngle();
+        
 
         transform.rotation = Quaternion.Lerp(transform.rotation, _targetRot, rotspeed * Time.deltaTime);
         ///
@@ -256,4 +430,6 @@ public class MoveAnimation : MonoBehaviour
 
 
     }
+
+    
 }
